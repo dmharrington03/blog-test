c19cae333422cd0126b641594270f58ff0cf2204
Daniel Harrington <dmharrington03@gmail.com>
2021-06-12 20:25:12 +0800

    Custom CSS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
-// import "./src/normalize.css"
+import "./src/normalize.css"
 // custom CSS styles
 import "./node_modules/uikit/dist/css/uikit.css"
 import "./src/customstyle.css"
